a2c4b0d2b6ce5899389c6108ca1516112a649a05
ISMAIL <isk28954@gmail.com>
2026-09-12 14:49:54 +0530

    Fix cv url

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -74,7 +74,7 @@ export default function Contact() {
 
     const link = document.createElement("a");
 
-    link.href = `${process.env.NEXT_PUBLIC_API_URL}${profile.cv_url}`;
+    link.href = `${profile.cv_url}`;
     link.target = "_blank";
     link.rel = "noopener noreferrer";
     link.download = "CV";
